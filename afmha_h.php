e12ba96fe1db39412064991806d9e2c14c505dce
Afmha <afmha@hotmail.com>
2016-06-19 12:54:13 +0300

    Update afmha_h.php

--- afmha_h.php
+++ afmha_h.php
@@ -1,19 +1,23 @@
 <?php
 
 	if($_GET){
-		if(isset($_GET['s1']) and isset($_GET['s2'])){
-		
-			define("SAYI1",$_GET['s1']);
-			define("SAYI2",$_GET['s2']);
-			$a = SAYI1+SAYI2; //Toplama
-			$b = SAYI1-SAYI2; //Çıkarma
-			$c = SAYI1*SAYI2; //Çarpma
-			$d = SAYI1/SAYI2; //Bölme
-			$e = SAYI1%SAYI2; //Modül
-			$f2 = SAYI2*100;
-			$f = $f2/SAYI1; //Yüzdelik
-			$h = array($a,$b,$c,$d,$e,$f);
-		
+		if(isset($_GET['s1']) and isset($_GET['s2'])){		
+			$s1=is_numeric($_GET['s1']);
+			$s2=is_numeric($_GET['s2']);
+			if($s1 and $s2){
+				define("SAYI1",$_GET['s1']);
+				define("SAYI2",$_GET['s2']);
+				$a = SAYI1+SAYI2; //Toplama
+				$b = SAYI1-SAYI2; //Çıkarma
+				$c = SAYI1*SAYI2; //Çarpma
+				$d = SAYI1/SAYI2; //Bölme
+				$e = SAYI1%SAYI2; //Modül
+				$f2 = SAYI2*100;
+				$f = $f2/SAYI1; //Yüzdelik
+				$h = array($a,$b,$c,$d,$e,$f);
+			}else{
+				$h = "HATA: 0";
+			}
 		}
 	}
 	
